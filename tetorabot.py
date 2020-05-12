b675f6585476de983d8705205e5b9f80f01f78a9
dorapon2000 <dorapon2000@gmail.com>
2020-05-12 23:09:23 +0900

    Remove template comment

--- tetorabot.py
+++ tetorabot.py
@@ -1,27 +1,20 @@
-# インストールした discord.py を読み込む
 import os
 import discord
 
 TOKEN = os.environ['DISCORD_APP_TOKEN']
-
-# 接続に必要なオブジェクトを生成
 client = discord.Client()
+client.run(TOKEN)
 
-# 起動時に動作する処理
+
 @client.event
 async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+    print('tetorabot is ready')
 
-# メッセージ受信時に動作する処理
+
 @client.event
 async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
+
     if message.content == '/neko':
         await message.channel.send('にゃーん')
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
